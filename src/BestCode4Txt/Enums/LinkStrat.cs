3dd72e2ffb3bf5c88d692d6dcf8c156af2e2c5f5
GarthTB | 天卜 <g-art-h@outlook.com>
2025-10-28 12:24:18 +0800

    添加连接策略枚举

---
+++ src/BestCode4Txt/Enums/LinkStrat.cs
@@ -0,0 +1,14 @@
+namespace BestCode4Txt.Enums;
+
+/// <summary> 编码及其开销的连接策略 </summary>
+internal enum LinkStrat
+{
+    /// <summary> 空格或标点 </summary>
+    SpaceOrPunct,
+
+    /// <summary> 无间隔 </summary>
+    NoGap,
+
+    /// <summary> 键道6顶功 </summary>
+    Jd6
+}
